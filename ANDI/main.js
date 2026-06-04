2e6f2a6db5404891d989e54e01c04b3d85395baf
ukaandi04-alt <ukaandi04@gmail.com>
2026-06-04 19:11:17 +0200

    andi commit

---
+++ ANDI/main.js
@@ -0,0 +1,30 @@
+function kontrolloMoshen(){
+    let mosha = prompt("Shkruaj moshen:")
+
+    if (mosha >=18) {
+        alert("Mund te shikosh filma +18 ne Cineplexx.");
+    } else {
+        alert ("Nuk mund te shikosh Filmat +18.");
+    }
+}
+
+let mosha = 12;
+
+if (mosha < 13) {
+    console.log("Bileta per Femije");
+} else if (mosha < 18) {
+    else {
+        console.log("Bileta per te rritur");
+    }
+}
+
+for (let salla = 1; salla <= 5; salla++) {
+    console.log(filmat[i]);
+}
+
+let bileta = 1;
+
+while (bileta <= 5) {
+    console.log("Bileta nr. + bileta");
+    bileta+++
+}
